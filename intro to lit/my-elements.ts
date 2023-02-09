73f89574cc6053c8b619c7aeed3d12db378cbb5d
KarenCouoh <karen.19070022@gmail.com>
2023-02-08 19:17:07 -0600

    into lit

--- intro to lit/my-elements.ts
+++ intro to lit/my-elements.ts
@@ -1,3 +1,15 @@
-import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
 
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+  @property()
+  message: string = 'Hello again.';
+
+  render() {
+    return html`
+      <p>${this.message}</p>
+    `;
+  }
+}
