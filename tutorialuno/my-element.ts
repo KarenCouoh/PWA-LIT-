418409bec65a36dde3c0dd51fb5326d0ef5b54db
KarenCouoh <karen.19070022@gmail.com>
2023-02-11 21:41:00 -0600

    elemento paso 3

--- tutorialuno/my-element.ts
+++ tutorialuno/my-element.ts
@@ -4,9 +4,12 @@ import {customElement} from 'lit/decorators.js';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
+    // TODO: Add a reactive property
   render() {
     return html`
       <p>Hello world! From my-element.</p>
     `;
   }
 }
+
+
