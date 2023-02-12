0c3cbb3f79a2740388fd68750500f4b1994ccd42
KarenCouoh <karen.19070022@gmail.com>
2023-02-11 21:51:55 -0600

    paso 5

--- tutorialuno/my-element.ts
+++ tutorialuno/my-element.ts
@@ -1,18 +1,23 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('name-tag')
-export class NameTag extends LitElement {
+@customElement('more-expressions')
+export class MoreExpressions extends LitElement {
   @property()
-  name: string = 'Your name here';
+  checked: boolean = false;
 
   render() {
-    // TODO: Add declarative event listener to input.
     return html`
-      <p>Hello, ${this.name}</p>
-      <input placeholder="Enter your name">
+      <div>
+         <!-- TODO: Add expression to input. -->
+         <input type="text" value="Hello there.">
+      </div>
+      <label><input type="checkbox" @change=${this.setChecked}> Enable editing</label>
     `;
   }
 
-  // TODO: Add event handler method.
+  setChecked(event: Event) {
+    this.checked = (event.target as HTMLInputElement).checked;
+  }
 }
+
