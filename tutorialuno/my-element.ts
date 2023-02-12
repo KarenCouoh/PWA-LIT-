1d77d49dc6d1d153824e0b4e2914b47d43895be9
KarenCouoh <karen.19070022@gmail.com>
2023-02-11 21:38:28 -0600

    elemento paso 3

--- tutorialuno/my-element.ts
+++ tutorialuno/my-element.ts
@@ -1,2 +1,12 @@
 import {LitElement, html} from 'lit';
 import {customElement} from 'lit/decorators.js';
+
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+  render() {
+    return html`
+      <p>Hello world! From my-element.</p>
+    `;
+  }
+}
