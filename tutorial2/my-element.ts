05972b5df9d5288162a247b25ad2a910d937fd99
KarenCouoh <karen.19070022@gmail.com>
2023-02-12 10:44:33 -0600

    paso2finish

--- tutorial2/my-element.ts
+++ tutorial2/my-element.ts
@@ -1,20 +1,14 @@
-import {LitElement, html} from 'lit';
-import {customElement, state} from 'lit/decorators.js';
-// TODO: import map directive.
-
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  items = new Set(['Apple', 'Banana', 'Grape', 'Orange', 'Lime'])
+  items = new Set(['Amarrillo', 'rojo', 'morado', 'negro', 'blanco'])
 
   render() {
     return html`
       <p>My unique fruits</p>
       <ul>
-        <!-- TODO: Utilize map directive to render items. -->
+        ${map(this.items, (item) => html`<li>${item}</li>`)}
       </ul>
     `;
   }
 }
-
-
